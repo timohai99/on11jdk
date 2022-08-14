e31799faee5bd7b98da2562228b086c7d8d8fc89
timohai99 <qwertyinvest99@gmail.com>
2022-08-14 21:39:01 +0300

    OkeR11

--- src/uchiJavaChak/dz5/Employee.java
+++ src/uchiJavaChak/dz5/Employee.java
@@ -7,6 +7,7 @@ public class Employee {
        let = age;
        zp = salary;
        otdel = department;
+        //выше создал конструктор в котором прописал параметры
    }
     int id;
     int age;
@@ -14,21 +15,22 @@ public class Employee {
     String department;
     double salary;
 
-    double uvelzp (double salary) {
-    double res = salary + salary;
-    return res;
+    //double uvelzp (double salary) { //на этот метод не обращай пока внимание
+    //double res = salary + salary;
+    //return res;
     }
-}
+//}
 
 class  EmployeeTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//создаю переменные а1. а2, а3 прописываю чо выводить в их конструкторе
         Employee a1 = new Employee(1,"Lobov",34,33000.50,"снабжения");
         Employee a2 = new Employee(2,"Goof",31,77000.00,"снабжения");
         Employee a3 = new Employee(3,"Ivanov",21,15600.00,"технический");
 
 
-
+// а выводит дефолтные значения переменных как будто бы не видит параметры в конструкторе
+        //не могу понять почему так происходит
+        System.out.println(a1.id);
         System.out.println(a1.age);
-        System.out.println(a2.age);
     }
 }
